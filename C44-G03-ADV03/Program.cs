10118d4dbc73b2bebb1cb49b8d7ac3f521f09286
ahmed <ahmed@AHMED_SAMY>
2025-08-29 16:28:00 +0300

    Part 01

--- C44-G03-ADV03/Program.cs
+++ C44-G03-ADV03/Program.cs
@@ -1,10 +1,32 @@
 ﻿namespace C44_G03_ADV03
 {
+    public delegate int StringFuncDelegate(string s);
+    // New Delegate [Class]
+    // Ref From Delegate can Refer To Function or More [Pointer to function]
+    // this function can be static Function [Class Member function]
+    // this function can be non-static Function [Object Member function]
+    // this function must be with same signature of delegate
+    // Regardless Function Naming [Method name - Paramter Name]
+    // Regardless Access Modifier
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //int X = StringFunctions.GetCountOfUpperCaseChar; // invalid
+
+            //StringFuncDelegate stringFunc = new StringFuncDelegate(StringFunctions.GetCountOfUpperCaseChar);
+
+            StringFuncDelegate? stringFunc = StringFunctions.GetCountOfUpperCaseChar;
+
+            stringFunc += StringFunctions.GetCountOfLowerCaseChar;
+            stringFunc -= StringFunctions.GetCountOfLowerCaseChar;
+            stringFunc -= StringFunctions.GetCountOfUpperCaseChar;
+
+
+            int Result = stringFunc?.Invoke("RouTe") ?? 0;
+            //Result = stringFunc("MostAfa HanY");
+            Console.WriteLine(Result);
         }
     }
 }
